ce7011067806ea547b6eebac32d230cef72b42af
Tuan792 <quoctuan2926@gmail.com>
2026-03-13 23:34:01 +0700

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,6 +70,10 @@ export const signup = async (req, res) => {
 export const login = async ( req, res) => {
     const {email, password} = req.body;
 
+    if (!email || !password) {
+        return res.status(400).json({message: "Phai dien email va mat khau"});
+    }
+
     try {
         const user = await User.findOne({email});
         if(!user) return res.status(400).json({message:"Thong tin dang nhap khong hop le"});
