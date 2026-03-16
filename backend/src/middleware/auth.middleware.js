ac1d5ea0a04c846341f843b5b6a12a04fe5f39b5
Tuan792 <quoctuan2926@gmail.com>
2026-03-16 13:22:15 +0700

    rate limiting implemented

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ import { ENV } from "../lib/env.js";
 export const protectRoute = async (req,res,next) => {
     try {
         const token = req.cookies.jwt;
-        if(!token) return res.status(410).json({message:"Khong co tac gia"});
+        if(!token) return res.status(401).json({message:"Khong co tac gia"});
 
         const decoded = jwt.verify(token,ENV.JWT_SECRET);
         if(!decoded) return res.status(401).json({message:"Khong co tac gia - kxd giau hieu"});
