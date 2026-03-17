e337f278c7d9c409914dcca06ce80696978df236
Tuan792 <quoctuan2926@gmail.com>
2026-03-18 01:52:57 +0700

    login&logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,6 +4,8 @@ import { axiosInstance } from "../lib/axios";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
     
     checkAuth: async () => {
         try {
@@ -24,11 +26,38 @@ export const useAuthStore = create((set, get) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
 
-            toast.success("Account created successfully!");
+            toast.success("Tạo tài khoản thành công");
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+    login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("Đăng nhập thành công");
+
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Đăng xuất thành công");
+
+    } catch (error) {
+      toast.error("Lỗi đăng xuất");
+      console.log("Logout error:", error);
     }
   },
 }));
