9318ce92fa1a7f73b8323e44ef657022284dcd87
Tuan792 <quoctuan2926@gmail.com>
2026-05-03 01:35:58 +0700

    chac la on

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -10,8 +10,9 @@ import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-//router.use(arcjetProtection);
-router.use(protectRoute);
+// the middlewares execute in order - so requests get rate-limited first, then authenticated.
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
+router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
