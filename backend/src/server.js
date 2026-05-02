2a0be758972618ee096eaa4d0c01518b58bcbe3b
Tuan792 <quoctuan2926@gmail.com>
2026-05-03 01:01:20 +0700

    Remove frontend static serving from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,11 +23,11 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+//  app.get("*", (_, res) => {
+//    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//  });
 }
 
 server.listen(PORT, () => {
