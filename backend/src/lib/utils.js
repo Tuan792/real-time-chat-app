447e4cb6444552ce17fd65358b9cc23361231cfa
Tuan792 <quoctuan2926@gmail.com>
2026-05-03 14:52:57 +0700

    sua loi

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,12 +11,12 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
-  });
+res.cookie("jwt", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
   return token;
 };
