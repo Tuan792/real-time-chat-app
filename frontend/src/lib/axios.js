d354284b5d05815467e161619e0f286d7f4cf2b4
Tuan792 <quoctuan2926@gmail.com>
2026-05-21 00:20:09 +0700

    chuan bi khoi dong

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const BASE_URL=import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "/api";
 
-const api = axios.create({
+const axiosInstance = axios.create({
   baseURL: BASE_URL,
-})
-export default api;
+});
+
+export default axiosInstance;
