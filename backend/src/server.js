40c365ad22c58871145bf7baf4ab29271c9e5e25
Tuan792 <quoctuan2926@gmail.com>
2026-05-20 23:53:52 +0700

    prepare deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,11 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-  withCredentials: true,
-});
+const BASE_URL=import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+})
+export default api;
