ad243e852aca8106024653e8c77e966b28a14530
Tuan792 <quoctuan2926@gmail.com>
2026-06-07 18:03:08 +0700

    some fix

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -6,18 +6,22 @@ function ActiveTabSwitch() {
   return (
     <div className="tabs tabs-boxed bg-transparent p-2 m-2">
       <button
-        onClick={() => setActiveTab("Tin nhắn")}
+        onClick={() => setActiveTab("chats")}
         className={`tab ${
-          activeTab === "Tin nhắn" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
+          activeTab === "chats"
+            ? "bg-cyan-500/20 text-cyan-400"
+            : "text-slate-400"
         }`}
       >
         Tin nhắn
       </button>
 
       <button
-        onClick={() => setActiveTab("Liên hệ")}
+        onClick={() => setActiveTab("contacts")}
         className={`tab ${
-          activeTab === "Liên hệ" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
+          activeTab === "contacts"
+            ? "bg-cyan-500/20 text-cyan-400"
+            : "text-slate-400"
         }`}
       >
         Liên hệ
@@ -25,4 +29,5 @@ function ActiveTabSwitch() {
     </div>
   );
 }
+
 export default ActiveTabSwitch;
